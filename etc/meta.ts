993e2a3c0ffd8417f0f3831f9d65d278194be9aa
Dmitry Balashov <a.marcius26@gmail.com>
2023-03-31 15:22:21 +0600

    [chore]: fix format
    Signed-off-by: Dmitry Balashov <a.marcius26@gmail.com>

--- etc/meta.ts
+++ etc/meta.ts
@@ -11,7 +11,7 @@ export const IROHA_REV_DEV = '726f5eabf65a79ea618b4fce62a09cee7a5b13d1'
 /**
  * hyperledger/iroha#iroha2-lts
  */
-export const IROHA_REV_LTS = '6e75248b84c73bf0415e1718024c75f406b5d354';
+export const IROHA_REV_LTS = '6e75248b84c73bf0415e1718024c75f406b5d354'
 
 /**
  * hyperledger/iroha-javascript#iroha2
