f8416024939381e32c69107946560f29caf7d416
Dmitry Balashov <a.marcius26@gmail.com>
2023-07-17 09:50:45 +0700

    [feat]: impl basic compat matrix functionality
    Signed-off-by: Dmitry Balashov <a.marcius26@gmail.com>

---
+++ .vitepress/theme/components/CompatibilityMatrixTable.vue
@@ -0,0 +1,125 @@
+<script setup lang="ts">
+import { useTask } from '@vue-kakuyaku/core'
+import { SSpinner } from '@soramitsu-ui/ui'
+import { computed } from 'vue'
+import IconCheck from './icons/IconCheck.vue'
+import IconCancelOutlineRounded from './icons/IconCancelOutlineRounded.vue'
+import IconQuestionMarkRounded from './icons/IconQuestionMarkRounded.vue'
+
+interface Matrix {
+  included_sdks: MatrixSdkDeclaration[]
+  stories: MatrixStory[]
+}
+
+interface MatrixSdkDeclaration {
+  name: string
+}
+
+interface MatrixStory {
+  name: string
+  results: MatrixStoryResult[]
+}
+
+interface MatrixStoryResult {
+  status: 'ok' | 'failed' | 'no-data'
+}
+
+const COMPAT_MATRIX_URL: string = import.meta.env.VITE_COMPAT_MATRIX_URL
+
+const task = useTask<Matrix>(
+  () => {
+    return fetch(COMPAT_MATRIX_URL, {}).then((x) => x.json())
+  },
+  { immediate: true },
+)
+
+const table = computed(() => {
+  if (!task.state.fulfilled) return null
+  const data = task.state.fulfilled.value
+
+  const headers = ['Story', ...data.included_sdks.map((x) => x.name)]
+  const rows = data.stories.map((story) => {
+    return { story: story.name, results: story.results.map((x) => x.status) }
+  })
+
+  return { headers, rows }
+})
+</script>
+
+<template>
+  <table v-if="table">
+    <thead>
+      <th
+        v-for="name in table.headers"
+        :key="name"
+      >
+        {{ name }}
+      </th>
+    </thead>
+    <tbody>
+      <tr
+        v-for="(row, i) in table.rows"
+        :key="i"
+      >
+        <td>{{ row.story }}</td>
+        <td
+          v-for="(status, j) in row.results"
+          :key="j"
+          class="status-cell"
+          :data-status="status"
+          :title="`Status: ${status}`"
+        >
+          <IconCheck v-if="status === 'ok'" />
+          <IconCancelOutlineRounded v-else-if="status === 'failed'" />
+          <IconQuestionMarkRounded v-else-if="status === 'no-data'" />
+          <template v-else>
+            {{ status }}
+          </template>
+        </td>
+      </tr>
+    </tbody>
+  </table>
+
+  <div
+    v-else
+    class="border rounded p-2 my-4"
+  >
+    <div
+      v-if="task.state.pending"
+      class="flex space-x-2 items-center"
+    >
+      <SSpinner /> <span>Loading data...</span>
+    </div>
+    <div v-else-if="task.state.rejected">
+      Failed to load compatibility matrix data: {{ task.state.rejected.reason }}
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.border {
+  border-color: var(--vp-c-border);
+}
+
+td.status-cell {
+  font-size: 1.3em;
+  padding: 0;
+
+  svg {
+    margin-left: auto;
+    margin-right: auto;
+  }
+
+  &[data-status='ok'] {
+    color: var(--vp-c-green);
+  }
+
+  &[data-status='failed'] {
+    color: var(--vp-c-red);
+  }
+
+  &[data-status='no-data'] {
+    color: var(--vp-c-yellow);
+  }
+}
+</style>
---
+++ src/compatibility-matrix.md
@@ -0,0 +1,3 @@
+# Compatibility Matrix
+
+<CompatibilityMatrixTable />
